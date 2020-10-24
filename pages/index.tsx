0dd400a78a0c5c017f1f69654060b05ee088ffa1
nonissue <andy@nonissue.org>
2020-10-23 19:10:11 -0600

    chore: cleanup before publish

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,33 +8,29 @@ import {
   FormErrorMessage,
   FormHelperText
 } from "@chakra-ui/core";
+import { FormEvent } from "react";
+import { useRouter } from "next/router";
 
 import { Layout } from "../components/Layout";
 
 const IndexPage: React.FunctionComponent = () => {
+  const router = useRouter();
   // const iconColor = useColorModeValue("gray.300", "gray.500");
   // const alertBGColor = useColorModeValue("gray.100", "gray.700");
   // const alertBorderColor = useColorModeValue("gray.200", "gray.600");
+
+  const handleSubmit = (event: FormEvent) => {
+    // alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+    router.push("/weights");
+    // alert("Logging in");
+  };
 
   return (
     <Layout>
       <Grid maxW="min(65ch, 100%)" mx="auto" px={["4", "4", "2", "2"]} my="0">
         <Stack spacing={3} my="4">
-          {/* <Alert
-            //
-            status="info"
-            // variant="info"
-            border="1px"
-            background={alertBGColor}
-            borderColor={alertBorderColor}
-            shadow="sm"
-            mb="5"
-            borderRadius={10}
-          >
-            <AlertIcon />
-            Authentication is required.
-          </Alert> */}
-          <form>
+          <form onSubmit={e => handleSubmit(e)}>
             <FormControl id="email" isRequired>
               <FormLabel>Enter Password</FormLabel>
               <Input type="password" />
@@ -43,10 +39,17 @@ const IndexPage: React.FunctionComponent = () => {
               </FormHelperText>
               <FormErrorMessage>Required</FormErrorMessage>
             </FormControl>
-
-            <Button mt={4} colorScheme="teal" isLoading={false} type="submit">
-              Login
-            </Button>
+            <Stack align="flex-end">
+              <Button
+                mt={4}
+                w={["100%", "20vw"]}
+                colorScheme="teal"
+                isLoading={false}
+                type="submit"
+              >
+                Login
+              </Button>
+            </Stack>
           </form>
         </Stack>
       </Grid>
