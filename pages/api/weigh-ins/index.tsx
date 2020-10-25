74f5d88026434543a69ee473af037b64c06bc9f3
nonissue <andy@nonissue.org>
2020-10-24 21:01:48 -0600

    feat: parse entries and insert multiple into db in api call

--- pages/api/weigh-ins/index.tsx
+++ pages/api/weigh-ins/index.tsx
@@ -6,27 +6,53 @@ import { FormResult, Entry } from "../../../interfaces";
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
+
 export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
   const prisma = new PrismaClient();
+
   const { date, entries } = req.body;
 
-  const result = entries.map(async (entry: Entry) => {
-    try {
-      const insert = await prisma.weighIn.create({
-        data: {
-          weighDate: date,
-          weight: parseInt(entry.weight),
-          person: { connect: { name: entry.name } }
-        }
-      });
-      console.log(insert);
-    } catch (e) {
-      console.log(e);
-    }
+  const records = entries.map((entry: Entry) => {
+    return prisma.weighIn.create({
+      data: {
+        weighDate: date,
+        weight: parseInt(entry.weight),
+        person: { connect: { name: entry.name } }
+      },
+      include: { person: true }
+    });
   });
+
+  let result;
+
+  try {
+    result = await prisma.$transaction(records);
+  } catch (e) {
+    console.log(e);
+  }
+
+  console.log(result);
+
+  // const result = Promise.all(records);
+
+  // const result = entries.map(async (entry: Entry) => {
+  //   try {
+  //     const insert = await prisma.weighIn.create({
+  //       data: {
+  //         weighDate: date,
+  //         weight: parseInt(entry.weight),
+  //         person: { connect: { name: entry.name } }
+  //       }
+  //     });
+
+  //     console.log(insert);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // });
 
   /*  const result = await prisma.weighIn.create({
     data: {
