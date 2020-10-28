d2ff52ddc53f059e2116eafb849adb8e88b33b5f
nonissue <andy@nonissue.org>
2020-10-27 22:50:53 -0600

    chore: cleanup, minor changes

--- pages/people/[name].tsx
+++ pages/people/[name].tsx
@@ -8,13 +8,14 @@ import {
   ListItem,
   Text,
   VStack,
+  Link,
   Divider,
   useColorModeValue
 } from "@chakra-ui/core";
 import { Layout } from "../../components/Layout";
+import { Person } from "../../interfaces";
 
 const prisma = new PrismaClient();
-// import { Heading, Divider, Box, List} from
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const result = await prisma.person.findOne({
@@ -29,10 +30,44 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 };
 
-export const PersonPage: React.FunctionComponent = ({ test }: any) => {
+export const PersonPage: React.FunctionComponent<{ test: string }> = ({
+  test
+}) => {
+  const data: Person = JSON.parse(test);
+
   const weightColor = useColorModeValue("gray.700", "gray.300");
 
-  const data = JSON.parse(test);
+  if (!data) {
+    return (
+      <Layout>
+        <Grid templateColumns={`1fr min(65ch, 100%) 1fr`}>
+          <Grid column="2" my="4" px={["4", "4", "2", "2"]} textAlign="center">
+            <Heading>Error 404</Heading>
+            <Heading fontSize="lg" fontWeight="500" mt="2">
+              Person Not Found
+            </Heading>
+            <Text mt="6">
+              <Link
+                href="mailto:andy@nonissue.org"
+                px="3"
+                py="2"
+                borderRadius="6px"
+                textColor="gray.100"
+                fontFamily="body"
+                fontWeight="700"
+                shadow="sm"
+                borderBottomColor="red.400"
+                borderBottomWidth="2px"
+                background="red.500"
+              >
+                Report Error
+              </Link>
+            </Text>
+          </Grid>
+        </Grid>
+      </Layout>
+    );
+  }
 
   console.log(data);
 
@@ -57,34 +92,35 @@ export const PersonPage: React.FunctionComponent = ({ test }: any) => {
 
           <Divider />
           <List>
-            {data.weighIns.map((weighIn: any) => {
-              return (
-                <ListItem key={weighIn.id}>
-                  <VStack isInline spacing={0} align="center">
-                    <Text
-                      fontSize="3xl"
-                      fontFamily="DM Mono"
-                      fontWeight="600"
-                      color={weightColor}
-                    >
-                      {weighIn.weight}
-                    </Text>
-                    <Text
-                      fontFamily="DM Sans"
-                      fontSize="lg"
-                      pl="0"
-                      textTransform="none"
-                      fontWeight="400"
-                      fontStyle="italic"
-                      color="gray.500"
-                    >
-                      &nbsp;lbs
-                    </Text>
-                  </VStack>
-                  <Divider />
-                </ListItem>
-              );
-            })}
+            {data.weighIns &&
+              data.weighIns?.map(weighIn => {
+                return (
+                  <ListItem key={weighIn.id}>
+                    <VStack isInline spacing={0} align="center">
+                      <Text
+                        fontSize="3xl"
+                        fontFamily="DM Mono"
+                        fontWeight="600"
+                        color={weightColor}
+                      >
+                        {weighIn.weight}
+                      </Text>
+                      <Text
+                        fontFamily="DM Sans"
+                        fontSize="lg"
+                        pl="0"
+                        textTransform="none"
+                        fontWeight="400"
+                        fontStyle="italic"
+                        color="gray.500"
+                      >
+                        &nbsp;lbs
+                      </Text>
+                    </VStack>
+                    <Divider />
+                  </ListItem>
+                );
+              })}
           </List>
         </Grid>
       </Grid>
