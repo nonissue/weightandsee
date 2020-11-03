acbd44d68c9a7c8a057c81a087b45e2b93602d6c
nonissue <andy@nonissue.org>
2020-11-03 15:50:06 -0700

    feat: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
+
 /* eslint-disable @typescript-eslint/ban-types */
-import { Chakra } from '../Chakra';
+import { Chakra } from "../Chakra";
 
-import type { NextComponentType, NextPageContext } from 'next';
-import type { NextRouter } from 'next/router';
+import type { NextComponentType, NextPageContext } from "next";
+import type { NextRouter } from "next/router";
 
-import './react-datepicker.css'; // can we move this elsewhere with next.js 10?
+import "./react-datepicker.css"; // can we move this elsewhere with next.js 10?
 
 export interface AppRenderProps {
   pageProps: object;
@@ -14,12 +18,27 @@ export interface AppRenderProps {
   cookies?: string;
 }
 
-const App: React.FunctionComponent<AppRenderProps> = ({ Component, pageProps, cookies }) => {
+const App: React.FunctionComponent<AppRenderProps> = ({
+  Component,
+  pageProps,
+  cookies,
+}) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Chakra cookies={cookies}>
       <Component {...pageProps} />
     </Chakra>
   );
-}
+};
 
 export default App;
