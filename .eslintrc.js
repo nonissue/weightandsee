25ab7b2622ecb0b0202e1838be7f0e19849c32c5
nonissue <andy@nonissue.org>
2020-11-04 00:51:57 -0700

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,19 +7,23 @@ module.exports = {
   root: true,
   parser: "@typescript-eslint/parser", // the TypeScript parser we installed earlier
   parserOptions: {
-    ecmaFeatures: { jsx: true } // Allows for the parsing of JSX
+    ecmaFeatures: { jsx: true }, // Allows for the parsing of JSX
   },
+  plugins: ["@typescript-eslint", "import", "react"],
   extends: [
     "eslint:recommended", // eslint default rules
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
     "plugin:@typescript-eslint/eslint-recommended", // eslint TypeScript rules (github.com/typescript-eslint/typescript-eslint)
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended", // eslint react rules (github.com/yannickcr/eslint-plugin-react)
-    "plugin:jsx-a11y/recommended" // accessibility plugin
+    "plugin:jsx-a11y/recommended", // accessibility plugin
   ],
   rules: {
     "react/prop-types": "off", // We turn off prop-types rule, as we will use TypeScript's types instead.
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
-    "jsx-a11y/anchor-is-valid": "off"
-  }
+    "jsx-a11y/anchor-is-valid": "off",
+  },
 };
