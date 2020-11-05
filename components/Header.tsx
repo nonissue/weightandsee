5d831df97b81ad0029637430eda2a9ae54350457
nonissue <andy@nonissue.org>
2020-11-04 19:40:40 -0700

    feat: add backdrop filter to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,12 @@ import Head from "next/head";
 
 export const Header: React.FunctionComponent = () => {
   const headerColor = useColorModeValue("pink.400", "pink.300");
-  const headerBg = useColorModeValue("gray.100", "gray.700");
-  const headerDs = useColorModeValue("xs", "md");
+  // const headerBg = useColorModeValue("gray.50", "gray.800");
+  const headerBg = useColorModeValue(
+    "hsla(210, 38%, 95%, 0.05)",
+    "hsla(230, 21%, 15%, 0.7)"
+  );
+  const headerDs = useColorModeValue("md", "lg");
   const [mobileNavShown, setMobileNavShown] = useState(false);
 
   return (
@@ -17,7 +21,22 @@ export const Header: React.FunctionComponent = () => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Box shadow={headerDs} mb="6" as="header" bg={headerBg}>
+      <Box
+        shadow={headerDs}
+        // mb="6"
+        as="header"
+        bg={headerBg}
+        position="sticky"
+        top="0"
+        // bg="hsla(0,0,10,0.9)"
+        // mb="12"
+        textDecoration="initial"
+        zIndex="1000"
+        style={{
+          backdropFilter: "blur(25px)",
+          WebkitBackdropFilter: "blur(15px)",
+        }}
+      >
         <Box maxW="min(65ch, 100%)" mx="auto" px={["4", "4", "2", "2"]}>
           <Stack
             isInline
