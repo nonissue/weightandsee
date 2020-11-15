d594a0c10b4a69f0196c3bfe4535b28e5788695f
nonissue <andy@nonissue.org>
2020-11-14 20:00:14 -0700

    feat: GET /api/weigh-in returns all weigh-ins

--- pages/api/weigh-ins/index.tsx
+++ pages/api/weigh-ins/index.tsx
@@ -1,69 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { Entry } from "../../../interfaces";
 import db from "prisma";
 const prisma = db.getInstance().prisma;
-// POST /api/post
+// POST /api/weigh-ins
 // Required fields in body: title
 // Optional fields in body: content
-
-// Handle updateCurrentWeight
-
+// Currently creates, but should also handle get and if it is get, get all
 export default async (
-  req: NextApiRequest,
+  _req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
-  const { date, entries, updateCurrentWeight } = req.body;
-
-  const records = entries
-    .filter((entry: Entry) => !!entry)
-    .map((entry: Entry) => {
-      return prisma.weighIn.create({
-        data: {
-          weighDate: date,
-          weight:
-            typeof entry.weight === "string"
-              ? parseFloat(entry.weight)
-              : entry.weight,
-          person: { connect: { name: entry.name } },
-        },
-        include: { person: true },
-      });
-    });
-
   let result;
-
   try {
-    result = await prisma.$transaction(records);
-  } catch (e) {
-    console.log(e);
+    result = await prisma.weighIn.findMany();
+  } catch (error) {
+    console.log(error);
+    res.statusCode = 501;
+    res.end();
   }
-
-  let updateWeightRes;
-
-  if (updateCurrentWeight) {
-    try {
-      const updateCurrentWeights = entries
-        .filter((entry: Entry) => !!entry)
-        .map((entry: Entry) => {
-          return prisma.person.update({
-            where: { name: entry.name },
-            data: {
-              currentWeight:
-                typeof entry.weight === "string"
-                  ? parseFloat(entry.weight)
-                  : entry.weight,
-            },
-          });
-        });
-      updateWeightRes = await prisma.$transaction(updateCurrentWeights);
-    } catch (e) {
-      console.log("Error updating current weight");
-      console.log(e);
-    }
-  }
-
-  console.log(result);
-
-  res.json([result, updateWeightRes]);
+  res.json(result);
 };
