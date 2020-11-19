763896c456087a0412a8046523c44a7a7b8280e6
nonissue <andy@nonissue.org>
2020-11-18 18:46:37 -0700

    feat: add auth guard

---
+++ lib/guards/ensureAuthenticated.ts
@@ -0,0 +1,27 @@
+import { getSession } from "next-auth/client";
+import { GetServerSideProps } from "next";
+
+export const ensureAuthenticated: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  console.log(context);
+  console.log(context.req.headers);
+  console.log("context.resolved " + context.resolvedUrl);
+
+  const { res } = context;
+
+  const prefix =
+    context.req.headers.host === "localhost:3000" ? "http://" : "https://";
+
+  if (!session) {
+    res.setHeader(
+      "location",
+      `/user/signin?callbackUrl=${prefix}${context.req.headers.host}${context.resolvedUrl}`
+    );
+    res.statusCode = 302;
+    res.end();
+  }
+
+  return {
+    props: {},
+  };
+};
