be46f5a7b932595faf98afbe6cdf1d6919959ad3
nonissue <andy@nonissue.org>
2020-11-18 19:01:27 -0700

    feat: redirect '/' -> people

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Uncomment the line below to enable basePath, pages and
+  // redirects will then have a path prefix (`/app` in this case)
+  //
+  // basePath: '/app',
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/people",
+        permanent: true,
+      },
+    ];
+  },
+};
