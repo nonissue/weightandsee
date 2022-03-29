d48412088ba657ffde6cbd88a3f47beeaf81449e
Andy Williams <andy@nonissue.org>
2022-03-29 00:49:11 -0600

    Upgrade prisma migrate from legacy to current, use new dev db (weightandsee-dev)

--- components/WeightTag.tsx
+++ components/WeightTag.tsx
@@ -1,43 +1,45 @@
 import { useColorModeValue, Stack, Text, StackProps } from "@chakra-ui/react";
-
+import { NextChakraLink } from "./NextChakraLink";
 // TODO: Accept stack props as {...props}
 
 export const WeightTag: React.FunctionComponent<
-  { weight: number } & StackProps
-> = ({ weight, ...chakraProps }) => {
+  { weight: number; weighInId?: number } & StackProps
+> = ({ weight, weighInId, ...chakraProps }) => {
   const weightColor = useColorModeValue("gray.700", "gray.300");
   const weightBGColor = useColorModeValue("gray.200", "gray.700");
   const weightBorderColor = useColorModeValue("gray.300", "gray.600");
   const lbsColor = useColorModeValue("gray.500", "gray.400");
 
   return (
-    <Stack
-      isInline
-      spacing="0"
-      borderBottom="1px"
-      px="1"
-      ml="4"
-      align="center"
-      borderRadius="4px"
-      background={weightBGColor}
-      fontSize="md"
-      shadow="sm"
-      borderBottomColor={weightBorderColor}
-      {...chakraProps}
-    >
-      <Text fontWeight="400" px={0} fontFamily="mono" color={weightColor}>
-        {weight.toFixed(1)}
-      </Text>
-      <Text
-        fontWeight="600"
-        fontFamily="heading"
-        fontSize="0.7em"
-        pt="0.2rem"
+    <NextChakraLink href={`weights/${weighInId}`}>
+      <Stack
+        isInline
+        spacing="0"
+        borderBottom="1px"
+        px="1"
+        ml="4"
         align="center"
-        color={lbsColor}
+        borderRadius="4px"
+        background={weightBGColor}
+        fontSize="md"
+        shadow="sm"
+        borderBottomColor={weightBorderColor}
+        {...chakraProps}
       >
-        &#8198;lbs
-      </Text>
-    </Stack>
+        <Text fontWeight="400" px={0} fontFamily="mono" color={weightColor}>
+          {weight.toFixed(1)}
+        </Text>
+        <Text
+          fontWeight="600"
+          fontFamily="heading"
+          fontSize="0.7em"
+          pt="0.2rem"
+          align="center"
+          color={lbsColor}
+        >
+          &#8198;lbs
+        </Text>
+      </Stack>
+    </NextChakraLink>
   );
 };
